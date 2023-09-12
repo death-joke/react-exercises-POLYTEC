bc68c58d40833a25a5c0875da33f2a40e3d26c95
kevin <kevin.leborgne@universite-paris-saclay.fr>
2023-09-12 11:22:13 +0200

    tp-3

--- src/App.js
+++ src/App.js
@@ -5,98 +5,100 @@
 //import {TodoWithFullHttp} from './tp22-todo-full-http/soluce/TodoWithFullHttp';
 
 // import MyApp from './tp19-todo-router/MyApp';
+import { MyFormFunction_03_init ,MyFormFunction_03_step1,MyFormFunction_03_step2,MyFormFunction_03_step3,MyFormFunction_03_step4,MyFormFunction_03_step5,MyFormFunction_03_step6,MyFormFunction_03_step7,MyFormFunction_03_step8} from "./tp03-props-defaultProps-input-unmanaged/init/MyFormFunction_03-init";
 
 function App() {
-    return (
-      <div className="container">
-          hello world
+  return (
+    <div className="container">
+      {/*tp-03*/}
+      {/* <MyFormFunction_03_init />
+      <MyFormFunction_03_init stringAttr="33" integerAttr={33}/> */}
+      {/* <MyFormFunction_03_step1 buttonTitle={'OKK'}/>
+       <MyFormFunction_03_step2 buttonTitle={'OKK2'}/>
+      <MyFormFunction_03_step3 buttonTitle={'OK3'}/>
+     <MyFormFunction_03_step4 buttonTitle={'OK4'}/>
+    <MyFormFunction_03_step5 buttonTitle={'OK5'}/> */}
+    {/* <MyFormFunction_03_step6 buttonTitle={'OK6'}/> */}
+  {/* <MyFormFunction_03_step7 buttonTitle={'OK7'}/> */}
+  <MyFormFunction_03_step8 buttonTitle={'OK7'}/>
 
-          {/*tp-03*/}
-          <MyFormFunction_03_init />
-                    {/*<MyFormFunction_03_init stringAttr="33" integerAttr={33}/>*/}
-          {/*<MyFormFunction_03_step1 buttonTitle={'OKK'}/>*/}
-          {/*<MyFormFunction_03_step2 buttonTitle={'OKK2'}/>*/}
-          {/*<MyFormFunction_03_step3 buttonTitle={'OK3'}/>*/}
-          {/*<MyFormFunction_03_step4 buttonTitle={'OK4'}/>*/}
-          {/*<MyFormFunction_03_step5_extra buttonTitle={'OK5'}/>*/}
-          {/*  refs */}
-          {/*<MyFormComponentForRef/>*/}
+      {/*  refs */}
+      {/*<MyFormComponentForRef/>*/}
 
-          {/*tp-04*/}
-          {/*<Counter/>*/}
-          {/*<CounterWithInitialValue initialCounter={22}/>*/}
+      {/*tp-04*/}
+      {/*<Counter/>*/}
+      {/*<CounterWithInitialValue initialCounter={22}/>*/}
 
-          {/*tp-05 mimicking hooks to show how they work DEMO*/}
-          {/*  recoding hooks by hand to understand their logic  */}
-          {/*<Counter/>*/}
-          {/*<CounterMimick/>*/}
-          {/*<ListOfThingsMimickMulti/>*/}
+      {/*tp-05 mimicking hooks to show how they work DEMO*/}
+      {/*  recoding hooks by hand to understand their logic  */}
+      {/*<Counter/>*/}
+      {/*<CounterMimick/>*/}
+      {/*<ListOfThingsMimickMulti/>*/}
 
-          {/*tp-06-list-of-things*/}
-          {/*<ListOfThingsOnKey13/>*/}
-          {/*<ListOfThingsOnSubmit/>*/}
-          {/*<ListOfThings_ASYNC_PB/>*/}
+      {/*tp-06-list-of-things*/}
+      {/*<ListOfThingsOnKey13/>*/}
+      {/*<ListOfThingsOnSubmit/>*/}
+      {/*<ListOfThings_ASYNC_PB/>*/}
 
-          {/*tp-07 tic-tac-toe*/}
-          {/*For tic-tac-toe,    remove import './milligram.css'; at the top!*/}
-          {/*<AppTicTacToeSoluce/>*/}
-          {/*<AppTicTacToeSoluceExtra3/>*/}
+      {/*tp-07 tic-tac-toe*/}
+      {/*For tic-tac-toe,    remove import './milligram.css'; at the top!*/}
+      {/*<AppTicTacToeSoluce/>*/}
+      {/*<AppTicTacToeSoluceExtra3/>*/}
 
-          {/*tp-08 lifting state up*/}
-          {/*<ThingsContainer/>*/}
+      {/*tp-08 lifting state up*/}
+      {/*<ThingsContainer/>*/}
 
-          {/*tp-09 todo                   remove import './milligram.css'; at the top! */}
-          {/*tp-10 todo multicomp         remove import './milligram.css'; at the top! */}
-          {/*<Todo/>*/}
+      {/*tp-09 todo                   remove import './milligram.css'; at the top! */}
+      {/*tp-10 todo multicomp         remove import './milligram.css'; at the top! */}
+      {/*<Todo/>*/}
 
-          {/*DEMOS*/}
-          {/*tp-11 todo multicomp memo    remove import './milligram.css'; at the top! */}
-          {/*tp-11 todo multicomp memo useTasks         ... same ...                   */}
-          {/*tp-11 todo multicomp optim   remove import './milligram.css'; at the top! */}
-          {/*<Todo/>*/}
+      {/*DEMOS*/}
+      {/*tp-11 todo multicomp memo    remove import './milligram.css'; at the top! */}
+      {/*tp-11 todo multicomp memo useTasks         ... same ...                   */}
+      {/*tp-11 todo multicomp optim   remove import './milligram.css'; at the top! */}
+      {/*<Todo/>*/}
 
-          {/*tp-12 list of things multi   re-add import './milligram.css'; at the top!*/}
-          {/*<MultiList/>*/}
+      {/*tp-12 list of things multi   re-add import './milligram.css'; at the top!*/}
+      {/*<MultiList/>*/}
 
-          {/*tp13 Context*/}
-          {/*<ThingsWrapper/>*/}
+      {/*tp13 Context*/}
+      {/*<ThingsWrapper/>*/}
 
-          {/*tp14-multi-list-context*/}
-          {/*<MultiListContext/>*/}
+      {/*tp14-multi-list-context*/}
+      {/*<MultiListContext/>*/}
 
-          {/*tp-15-todo-context remove import './milligram.css'; at the top!*/}
-          {/*<Todo/>*/}
+      {/*tp-15-todo-context remove import './milligram.css'; at the top!*/}
+      {/*<Todo/>*/}
 
-          {/*tp16-demo-lifecycle-hooks-FC*/}
-          {/*<SimpleUseEffect01/>*/}
-          {/*<SimpleNoUseEffect02/>*/}
-          {/*<SimpleUseEffect03/>*/}
-          {/*For LifeCycle demo, remove StrictMode in index.js !*/}
-          {/*<AppDemoChildOnlyLifeCycle/>*/}
-          {/*demo with full logs is too long, so above is more straightforward and manageable */}
+      {/*tp16-demo-lifecycle-hooks-FC*/}
+      {/*<SimpleUseEffect01/>*/}
+      {/*<SimpleNoUseEffect02/>*/}
+      {/*<SimpleUseEffect03/>*/}
+      {/*For LifeCycle demo, remove StrictMode in index.js !*/}
+      {/*<AppDemoChildOnlyLifeCycle/>*/}
+      {/*demo with full logs is too long, so above is more straightforward and manageable */}
 
-          {/*tp17-counter-effect*/}
-          {/*<CounterEffectWithInterval/>*/}
+      {/*tp17-counter-effect*/}
+      {/*<CounterEffectWithInterval/>*/}
 
-          {/*tp18-router-simple*/}
-          {/*<MyAppOldRouterApi/>*/}
-          {/*<MyAppNewRouterApi/>*/}
+      {/*tp18-router-simple*/}
+      {/*<MyAppOldRouterApi/>*/}
+      {/*<MyAppNewRouterApi/>*/}
 
-          {/*tp19-todo-router*/}
-          {/*<MyRoutedTodoApp/>*/}
+      {/*tp19-todo-router*/}
+      {/*<MyRoutedTodoApp/>*/}
 
-          {/*tp21-todo-single-page-http*/}
-          {/*Launch the start.api*/}
-          {/*remove import './milligram.css'; at the top!*/}
-          {/*<TodoWithHttp/>*/}
+      {/*tp21-todo-single-page-http*/}
+      {/*Launch the start.api*/}
+      {/*remove import './milligram.css'; at the top!*/}
+      {/*<TodoWithHttp/>*/}
 
-          {/*tp22-todo-full-http*/}
-          {/*Launch the start.api*/}
-          {/*remove import './milligram.css'; at the top!*/}
-          {/* <TodoWithFullHttp/> */}
-
-      </div>
-    );
+      {/*tp22-todo-full-http*/}
+      {/*Launch the start.api*/}
+      {/*remove import './milligram.css'; at the top!*/}
+      {/* <TodoWithFullHttp/> */}
+    </div>
+  );
 }
 
 // for ListOfThingsMimickMulti to work you have to remove StrictMode in index.js !
--- src/tp03-props-defaultProps-input-unmanaged/init/MyFormFunction_03-init.js
+++ src/tp03-props-defaultProps-input-unmanaged/init/MyFormFunction_03-init.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 let count = 0;
 
 /**
@@ -5,23 +7,396 @@ let count = 0;
  * <MyFormFunction_01 stringAttr="33" integerAttr={33}/>
  */
 export function MyFormFunction_03_init(props) {
+  console.log("REFRESH MyFormFunction_01", count++);
 
-    console.log('REFRESH MyFormFunction_01', count++);
+  console.log("integerAttr", props.integerAttr, typeof props.integerAttr);
+  console.log("stringAttr", props.stringAttr, typeof props.stringAttr);
 
-    console.log('integerAttr', props.integerAttr, typeof props.integerAttr);
-    console.log('stringAttr', props.stringAttr, typeof props.stringAttr);
+  // Changing a prop will crash!
+  // props.stringAttr = 'pipoChange';
 
-
-    // Changing a prop will crash!
-    // props.stringAttr = 'pipoChange';
-
-    return (
-      <div>
-          <form noValidate autoComplete="off">
-              <label htmlFor="name">Name:</label>
-              <input id="name" type="number"/>
-              <button>OK</button>
-          </form>
-      </div>
-    );
+  return (
+    <div>
+      <form noValidate autoComplete="off">
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="number" />
+        <button>{props.buttonTitle}</button>
+      </form>
+    </div>
+  );
 }
+
+/**
+ * This example test the props types.
+ * <MyFormFunction_01 stringAttr="33" integerAttr={33}/>
+ */
+export function MyFormFunction_03_step1(props) {
+  console.log("REFRESH MyFormFunction_01", count++);
+
+  console.log("integerAttr", props.integerAttr, typeof props.integerAttr);
+  console.log("stringAttr", props.stringAttr, typeof props.stringAttr);
+
+  // Changing a prop will crash!
+  // props.stringAttr = 'pipoChange';
+
+  return (
+    <div>
+      <form noValidate autoComplete="off">
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="number" />
+        <button
+          onClick={() => {
+            console.log("i have been hit");
+          }}
+        >
+          {props.buttonTitle}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+/**
+ * This example test the props types.
+ * <MyFormFunction_01 stringAttr="33" integerAttr={33}/>
+ */
+export function MyFormFunction_03_step2(props) {
+  console.log("REFRESH MyFormFunction_01", count++);
+
+  console.log("integerAttr", props.integerAttr, typeof props.integerAttr);
+  console.log("stringAttr", props.stringAttr, typeof props.stringAttr);
+
+  // Changing a prop will crash!
+  // props.stringAttr = 'pipoChange';
+
+  return (
+    <div>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="number" />
+        <button
+          onClick={() => {
+            console.log("i have been hit");
+          }}
+        >
+          {props.buttonTitle}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+/**
+ * This example test the props types.
+ * <MyFormFunction_01 stringAttr="33" integerAttr={33}/>
+ */
+export function MyFormFunction_03_step3(props) {
+  console.log("REFRESH MyFormFunction_01", count++);
+
+  console.log("integerAttr", props.integerAttr, typeof props.integerAttr);
+  console.log("stringAttr", props.stringAttr, typeof props.stringAttr);
+  let name = 0;
+
+  // Changing a prop will crash!
+  // props.stringAttr = 'pipoChange';
+
+  return (
+    <div>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="number"
+          onChange={(e) => {
+            console.log(e.target.value);
+            name = e.target.value;
+          }}
+        />
+        <button
+          onClick={() => {
+            console.log("i have been hit value : ", name);
+          }}
+        >
+          {props.buttonTitle}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+/**
+ * This example test the props types.
+ * <MyFormFunction_01 stringAttr="33" integerAttr={33}/>
+ */
+export function MyFormFunction_03_step4(props) {
+  console.log("REFRESH MyFormFunction_01", count++);
+
+  console.log("integerAttr", props.integerAttr, typeof props.integerAttr);
+  console.log("stringAttr", props.stringAttr, typeof props.stringAttr);
+  let name;
+
+  // Changing a prop will crash!
+  // props.stringAttr = 'pipoChange';
+
+  return (
+    <div>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="number"
+          onChange={(e) => {
+            console.log(e.target.value);
+            name = e.target.value;
+          }}
+        />
+        <div>VALUE from Input Name : {name ? name : "empty"}</div>
+        <button
+          onClick={() => {
+            console.log("i have been hit value : ", name);
+          }}
+        >
+          {props.buttonTitle}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+/**
+ * This example test the props types.
+ * <MyFormFunction_01 stringAttr="33" integerAttr={33}/>
+ */
+export function MyFormFunction_03_step5(props) {
+  console.log("REFRESH MyFormFunction_01", count++);
+
+  console.log("integerAttr", props.integerAttr, typeof props.integerAttr);
+  console.log("stringAttr", props.stringAttr, typeof props.stringAttr);
+  let name;
+
+  // Changing a prop will crash!
+  // props.stringAttr = 'pipoChange';
+
+  return (
+    <div>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="number"
+          onChange={(e) => {
+            console.log(e.target.value);
+            name = e.target.value;
+          }}
+        />
+        <div>VALUE from Input Name : {name ? name : "empty"}</div>
+        <button
+          onClick={() => {
+            console.log("i have been hit value : ", name);
+          }}
+        >
+          {props.buttonTitle}
+        </button>
+      </form>
+      <MyChildFunction name={name} />
+    </div>
+  );
+}
+
+export const MyChildFunction = (props) => {
+  return (
+    <div>
+      <div>VALUE in CHILD from Input Name : {props.name}</div>
+    </div>
+  );
+};
+
+/**
+ * This example test the props types.
+ * <MyFormFunction_01 stringAttr="33" integerAttr={33}/>
+ */
+export function MyFormFunction_03_step6(props) {
+  const [name, setName] = useState(0);
+
+  // Changing a prop will crash!
+  // props.stringAttr = 'pipoChange';
+
+  return (
+    <div>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="number"
+          onChange={(e) => {
+            console.log(e.target.value);
+            setName(e.target.value);
+          }}
+        />
+        <div>VALUE from Input Name : {name ? name : "empty"}</div>
+        <button
+          onClick={() => {
+            setName((name) => name + 1);
+          }}
+        >
+          click me
+        </button>
+        <button
+          onClick={() => {
+            setName(0);
+          }}
+        >
+          reset
+        </button>
+      </form>
+      <MyChildFunction name={name} />
+    </div>
+  );
+}
+
+//create a componnet list where the user can add and remove items
+export function MyFormFunction_03_step7(props) {
+  const [liste, setListe] = useState([]);
+  const [name, setName] = useState(0);
+
+  return (
+    <div>
+      <form noValidate autoComplete="off">
+        <label htmlFor="name">New item:</label>
+        <input
+          id="name"
+          type="text"
+          onChange={(e) => {
+            console.log(e.target.value);
+            setName(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setListe((liste) => [...liste, name]);
+          }}
+        >
+          add
+        </button>
+      </form>
+
+      <div>
+        <ul>
+          {liste.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+/*************Version à plusieur composant  */
+
+export function MyFormFunction_03_step8(props) {
+  const [liste, setListe] = useState([]);
+  const [name, setName] = useState(0);
+
+  const addItemToList = () => {
+    setListe((liste) => [...liste, name]);
+  };
+
+  const resetItemList = () => {
+    setListe([]);
+  };
+
+  return (
+    <div>
+      <InputSection setName={setName} />
+      <ButtonSection
+        addItemToList={addItemToList}
+        resetItemList={resetItemList}
+      />
+      <ListSection liste={liste} />
+    </div>
+  );
+}
+
+export function InputSection(props) {
+  return (
+    <div>
+      <label htmlFor="name">New item:</label>
+      <input
+        id="name"
+        type="text"
+        onChange={(e) => {
+          console.log(e.target.value);
+          props.setName(e.target.value);
+        }}
+      ></input>
+    </div>
+  );
+}
+
+export function ButtonSection(props) {
+  return (
+    <div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          props.addItemToList();
+        }}
+      >
+        add
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          props.resetItemList();
+        }}
+      >
+        reset
+      </button>
+    </div>
+  );
+}
+
+export function ListSection(props) {
+  return (
+    <div>
+      <ul>
+        {props.liste.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+/********************************************* */
